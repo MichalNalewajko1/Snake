49016afdf03484458e861880a1d47291c711228f
MichalNalewajko1 <michaln130@gmail.com>
2026-07-24 17:07:10 +0200

    DOdanie projektu SNake, dodanie algorytmu poruszania sie

---
+++ mainwindow.cpp
@@ -0,0 +1,98 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    scene = new QGraphicsScene(0, 0, BOARD_WIDTH, BOARD_HEIGHT, this);
+    scene->setBackgroundBrush(QBrush(QColor("#1e1e2e")));
+    view = new QGraphicsView(scene, this);
+
+    view->setFixedSize(BOARD_WIDTH + 5, BOARD_HEIGHT + 5);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setCentralWidget(view);
+    setWindowTitle("Snake Game - C++ & Qt");
+    setFixedSize(BOARD_WIDTH + 20, BOARD_HEIGHT + 20);
+
+    currentDirection = Direction::RIGHT;
+    initSnake();
+
+    gameTimer = new QTimer(this);
+    connect(gameTimer, &QTimer::timeout, this, &MainWindow::moveSnake);
+    gameTimer->start(150);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::initSnake()
+{
+    int startX = 100;
+    int startY = 100;
+
+    for (int i = 0; i < 3; ++i) {
+        QGraphicsRectItem *segment = scene->addRect(0, 0, GRID_SIZE - 1, GRID_SIZE - 1,
+                                                    QPen(Qt::NoPen),
+                                                    QBrush(QColor(166, 227, 161)));
+
+        segment->setPos(startX - (i * GRID_SIZE), startY);
+
+        snake.append(segment);
+    }
+}
+
+void MainWindow::moveSnake()
+{
+    if (snake.isEmpty()) return;
+
+    QPointF headPos = snake.first()->pos();
+    qreal newX = headPos.x();
+    qreal newY = headPos.y();
+
+    switch (currentDirection) {
+    case Direction::UP:    newY -= GRID_SIZE; break;
+    case Direction::DOWN:  newY += GRID_SIZE; break;
+    case Direction::LEFT:  newX -= GRID_SIZE; break;
+    case Direction::RIGHT: newX += GRID_SIZE; break;
+    }
+
+    for (int i = snake.size() - 1; i > 0; --i) {
+        snake[i]->setPos(snake[i - 1]->pos());
+    }
+    snake.first()->setPos(newX, newY);
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+    case Qt::Key_Up:
+    case Qt::Key_W:
+        if (currentDirection != Direction::DOWN)
+            currentDirection = Direction::UP;
+        break;
+    case Qt::Key_Down:
+    case Qt::Key_S:
+        if (currentDirection != Direction::UP)
+            currentDirection = Direction::DOWN;
+        break;
+    case Qt::Key_Left:
+    case Qt::Key_A:
+        if (currentDirection != Direction::RIGHT)
+            currentDirection = Direction::LEFT;
+        break;
+    case Qt::Key_Right:
+    case Qt::Key_D:
+        if (currentDirection != Direction::LEFT)
+            currentDirection = Direction::RIGHT;
+        break;
+    default:
+        QMainWindow::keyPressEvent(event);
+        break;
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,49 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsRectItem>
+#include <QTimer>
+#include <QKeyEvent>
+#include <QList>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+enum class Direction { UP, DOWN, LEFT, RIGHT };
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+
+private slots:
+    void moveSnake();
+private:
+    Ui::MainWindow *ui;
+
+    QGraphicsScene *scene;
+    QGraphicsView *view;
+
+    const int BOARD_WIDTH = 600;
+    const int BOARD_HEIGHT = 600;
+    const int GRID_SIZE = 20;
+
+    QList<QGraphicsRectItem*> snake;
+    Direction currentDirection;
+    QTimer *gameTimer;
+
+    void initSnake();
+};
+#endif // MAINWINDOW_H
